aa79dd75af219dbcc5947220a049c4f05753b562
PLFJY <zero@plfjy.top>
2026-09-11 18:44:26 +0800

    packaging: vendor portal Meson subprojects

--- tests/test_packaging_policy.py
+++ tests/test_packaging_policy.py
@@ -8,6 +8,10 @@ PKGBUILD = (ROOT / "packaging/credentialsd-firefox-sidecar-git/PKGBUILD").read_t
 
 assert "/home/plfjy" not in PKGBUILD
 assert "git+https://github.com/PLFJY/credentialsd.git" in PKGBUILD
+assert "git+https://github.com/GNOME/libglnx.git#commit=ff64d52116ae74f0d25e24f089db28921ea171ff" in PKGBUILD
+assert "git+https://github.com/GNOME/gvdb.git#commit=c6f2359cc1d00f16e0a0e2527fa0bc1882b8b5ab" in PKGBUILD
+assert 'ln -s "$srcdir/libglnx" subprojects/libglnx' in PKGBUILD
+assert 'ln -s "$srcdir/gvdb" subprojects/gvdb' in PKGBUILD
 assert "io.github.PLFJY.CredentialPortal" in PKGBUILD
 assert "-Dfirefox_portal_bus_name=io.github.PLFJY.CredentialPortal" in PKGBUILD
 assert "-Dcargo_lto" not in PKGBUILD
